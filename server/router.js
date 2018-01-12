a3998c2c23e3109b2a6a58dc3dd52cdd32df4343
thomasgillot <thomas.gillot2@gmail.com>
2018-01-12 09:58:07 +0100

    add test controller

--- server/router.js
+++ server/router.js
@@ -12,6 +12,8 @@ const ROLE_ADMIN = require('./constants').ROLE_ADMIN;
 
 const passportService = require('./config/passport');
 
+const TestServerController = require('./controllers/test-server');
+
 // Middleware to require login/auth
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
@@ -19,99 +21,104 @@ const requireLogin = passport.authenticate('local', { session: false });
 module.exports = function (app) {
   // Initializing route groups
   const apiRoutes = express.Router(),
-    authRoutes = express.Router(),
-    userRoutes = express.Router(),
-    chatRoutes = express.Router(),
-    payRoutes = express.Router(),
-    communicationRoutes = express.Router();
+    testServerRoutes = express.Router()
+
+
+  apiRoutes.use('/test', testServerRoutes);
+
+  testServerRoutes.get('/:name', TestServerController.verifParam, TestServerController.displayMessage);
+
+  apiRoutes.get('/test1', requireAuth, (req, res) => {
+    res.send({ content: 'This is test 1' });
+  });
 
   //= ========================
   // Auth Routes
   //= ========================
 
   // Set auth routes as subgroup/middleware to apiRoutes
-  apiRoutes.use('/auth', authRoutes);
+  // apiRoutes.use('/auth', authRoutes);
 
   // Registration route
-  authRoutes.post('/register', AuthenticationController.register);
+  // authRoutes.post('/register', AuthenticationController.register);
 
   // Login route
-  authRoutes.post('/login', requireLogin, AuthenticationController.login);
+  // authRoutes.post('/login', requireLogin, AuthenticationController.login);
 
   // Password reset request route (generate/send token)
-  authRoutes.post('/forgot-password', AuthenticationController.forgotPassword);
+  // authRoutes.post('/forgot-password', AuthenticationController.forgotPassword);
 
   // Password reset route (change password using token)
-  authRoutes.post('/reset-password/:token', AuthenticationController.verifyToken);
+  // authRoutes.post('/reset-password/:token', AuthenticationController.verifyToken);
 
   //= ========================
   // User Routes
   //= ========================
 
   // Set user routes as a subgroup/middleware to apiRoutes
-  apiRoutes.use('/user', userRoutes);
+  // apiRoutes.use('/user', userRoutes);
 
   // View user profile route
-  userRoutes.get('/:userId', requireAuth, UserController.viewProfile);
+  // userRoutes.get('/:userId', requireAuth, UserController.viewProfile);
 
   // Test protected route
-  apiRoutes.get('/protected', requireAuth, (req, res) => {
-    res.send({ content: 'The protected test route is functional!' });
-  });
+  // apiRoutes.get('/protected', requireAuth, (req, res) => {
+  //   res.send({ content: 'The protected test route is functional!' });
+  // });
 
-  apiRoutes.get('/admins-only', requireAuth, AuthenticationController.roleAuthorization(ROLE_ADMIN), (req, res) => {
-    res.send({ content: 'Admin dashboard is working.' });
-  });
+  // apiRoutes.get('/admins-only', requireAuth, AuthenticationController.roleAuthorization(ROLE_ADMIN), (req, res) => {
+  //   res.send({ content: 'Admin dashboard is working.' });
+  // });
 
   //= ========================
   // Chat Routes
   //= ========================
 
   // Set chat routes as a subgroup/middleware to apiRoutes
-  apiRoutes.use('/chat', chatRoutes);
+  // apiRoutes.use('/chat', chatRoutes);
 
   // View messages to and from authenticated user
-  chatRoutes.get('/', requireAuth, ChatController.getConversations);
+  // chatRoutes.get('/', requireAuth, ChatController.getConversations);
 
   // Retrieve single conversation
-  chatRoutes.get('/:conversationId', requireAuth, ChatController.getConversation);
+  // chatRoutes.get('/:conversationId', requireAuth, ChatController.getConversation);
 
   // Send reply in conversation
-  chatRoutes.post('/:conversationId', requireAuth, ChatController.sendReply);
+  // chatRoutes.post('/:conversationId', requireAuth, ChatController.sendReply);
 
   // Start new conversation
-  chatRoutes.post('/new/:recipient', requireAuth, ChatController.newConversation);
+  // chatRoutes.post('/new/:recipient', requireAuth, ChatController.newConversation);
 
   //= ========================
   // Payment Routes
   //= ========================
-  apiRoutes.use('/pay', payRoutes);
+  // apiRoutes.use('/pay', payRoutes);
 
-  // Webhook endpoint for Stripe
-  payRoutes.post('/webhook-notify', StripeController.webhook);
+  // // Webhook endpoint for Stripe
+  // payRoutes.post('/webhook-notify', StripeController.webhook);
 
-  // Create customer and subscription
-  payRoutes.post('/customer', requireAuth, StripeController.createSubscription);
+  // // Create customer and subscription
+  // payRoutes.post('/customer', requireAuth, StripeController.createSubscription);
 
-  // Update customer object and billing information
-  payRoutes.put('/customer', requireAuth, StripeController.updateCustomerBillingInfo);
+  // // Update customer object and billing information
+  // payRoutes.put('/customer', requireAuth, StripeController.updateCustomerBillingInfo);
 
-  // Delete subscription from customer
-  payRoutes.delete('/subscription', requireAuth, StripeController.deleteSubscription);
+  // // Delete subscription from customer
+  // payRoutes.delete('/subscription', requireAuth, StripeController.deleteSubscription);
 
-  // Upgrade or downgrade subscription
-  payRoutes.put('/subscription', requireAuth, StripeController.changeSubscription);
+  // // Upgrade or downgrade subscription
+  // payRoutes.put('/subscription', requireAuth, StripeController.changeSubscription);
 
-  // Fetch customer information
-  payRoutes.get('/customer', requireAuth, StripeController.getCustomer);
+  // // Fetch customer information
+  // payRoutes.get('/customer', requireAuth, StripeController.getCustomer);
 
-  //= ========================
-  // Communication Routes
-  //= ========================
-  apiRoutes.use('/communication', communicationRoutes);
+  // //= ========================
+  // // Communication Routes
+  // //= ========================
+  // apiRoutes.use('/communication', communicationRoutes);
 
-  // Send email from contact form
-  communicationRoutes.post('/contact', CommunicationController.sendContactForm);
+  // // Send email from contact form
+  // communicationRoutes.post('/contact', CommunicationController.sendContactForm);
 
   // Set url for API group routes
   app.use('/api', apiRoutes);
