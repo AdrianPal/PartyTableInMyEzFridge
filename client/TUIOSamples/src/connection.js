7b7f46afe036486a9aac7ca864b2191859047a65
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-21 06:06:03 +0100

    Integration (not really working but it's a start) :smirk:

---
+++ client/TUIOSamples/src/connection.js
@@ -0,0 +1,73 @@
+//contient le code js qu'il y avait dans index.html
+
+const apiUrl = "http://localhost:3000";
+
+let usersCount = 0,
+    gameId = null;
+
+var socket = io.connect('http://172.20.10.3:3000');
+
+socket.on('refresh game', function (d) {
+    d.game.forEach(function(e) {
+        $('#username_'+e.pos).html('<b>'+e.name+'</b>');
+        $('#qrcode_'+e.pos).hide();
+    });
+});
+
+function connectSocketForCurrentGame() {
+    socket.emit('enter game', gameId);
+}
+
+function getLink() {
+    return location.href.replace(/index\.html/i, "");
+}
+
+$(function () {
+    $('#newGame').on('click', function (e) {
+        e.preventDefault();
+
+        let $that = $(this);
+
+        $.post(apiUrl + '/api/game')//problem here ?🤔🤔🤔🤔🤔🤔
+            .done(function (d) {
+                $('#addUser').removeClass('d-none');
+                gameId = d.gameId;
+                $that.hide();
+
+                connectSocketForCurrentGame();
+            })
+            .fail(function (e) {
+                alert('Error');
+                console.log(e);
+            });
+    });
+
+    $('#addUser').on('click', function (e) {
+        e.preventDefault();
+
+        ++usersCount;
+
+        let link = getLink() + './user.html?id=' +usersCount + '&gameId=' + gameId;
+        
+        $('#users').append('<h3>'+ usersCount +':</h3> <a href="'+ link +'"><div id="qrcode_'+ usersCount +'"></div></a><span id="username_'+ usersCount +'"></span><hr>');
+
+        new QRCode('qrcode_' + usersCount, link);
+    });
+});
+
+// function send() {
+//     var name = document.getElementById('method').value;
+//     var params = document.getElementById('params').value;
+
+//     socket.emit(name, params);
+// }
+
+// socket.emit('hello', null);
+
+// socket.on('hello', function(msg){
+//     document.getElementById('test').innerHTML += '<br>'+msg;
+// });
+
+// socket.on('helloall', function(msg){
+//     document.getElementById('test').innerHTML += '<br>'+msg;
+// });
--- client/TUIOSamples/src/menu.js
+++ client/TUIOSamples/src/menu.js
@@ -15,50 +15,26 @@ import CircularMenu from 'tuiomanager/widgets/CircularMenu/CircularMenu';
 import LibraryStack from 'tuiomanager/widgets/Library/LibraryStack/LibraryStack';
 import MenuItem from 'tuiomanager/widgets/CircularMenu/MenuItem';
 
+//fonction appelée dans index.js, le script de base de la page
 export default function buildMenu() {
-  $('#app').append('<div id="startView"></div>');
+  $('#app').append('<div id="startView"></div>'); //On ajoute la vue de départ
   
-  $('#startView').append('<h1 id="title"> Party Table In my EZ Fridge </h1>');
+  //Ajout du html qui était précédemment dans index.html
+  $('#startView').append('<h1 id="title"> Party Table In my EZ Fridge </h1>'+
+  ' <div id="main">'+
+  '   <button class="btn btn-primary" id="newGame">New game</button>'+
+
+  '   <button class="btn btn-success d-none" id="addUser">Add participant</button>'+
+
+  '   <div id="users"></div>'+
+  ' </div>'
+);
 
   //THIS IS A TEST BUTTON, just used to test animations and functions
   $('#startView').append('<button id="trigger">Trigger me</button>');  
   $('#trigger').click(function(){
-    hideQRCode($('#qrcodeR'));
+    
   });
-  buildQRCodes();
  
 }// buildMenu()
 
-
-
-function buildQRCodes()
-{
-
-  $('#title').css('marginTop', ($(window).height() - $('#title').height())/2);
-
-  $('#startView').append('<img src="../assets/qr.jpg" class="qrcode" id="qrcodeL"/>');
-  $('#startView').append('<img src="../assets/qr.jpg" class="qrcode" id="qrcodeR"/>');
-  $('#startView').append('<img src="../assets/qr.jpg" class="qrcode" id="qrcodeT"/>');
-  $('#startView').append('<img src="../assets/qr.jpg" class="qrcode" id="qrcodeB"/>');
-
-  $('#qrcodeR').css('top', ($(window).height() - $('#qrcodeR').height())/2 );
-  $('#qrcodeL').css('top', ($(window).height() - $('#qrcodeL').height())/2 );
-  $('#qrcodeT').css('left', ($(window).width() - $('#qrcodeT').width())/2 );
-  $('#qrcodeB').css('left', ($(window).width() - $('#qrcodeB').width())/2 );
-  
-}
-
-function hideQRCode(qrcode)
-{
-  qrcode.transition({ scale: 0, duration:'600' });
-}
-
-
-///Have to replace this shyte
-var rot = 360;
-
-setInterval(function(){ 
-  $('#title').transition({ rotate: rot+'deg', duration:'10000' }, 'snap'); 
-  rot +=360;  
-  console.log("rotated"); 
-}, 10000);
