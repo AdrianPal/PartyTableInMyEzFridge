5ba4bbf7e18e4ed105c50b0d531eed4e75ebd2e2
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 13:29:37 +0100

    Adding the labyrinth file and launching it

--- client/TUIOSamples/src/games/labyrinth.js
+++ client/TUIOSamples/src/games/labyrinth.js
@@ -2,3 +2,9 @@
  * @author: Arnaud ZAGO
  * 
  */
+
+export default function launchLabyrinth(players)
+{
+   $('#boardView').remove();
+   $('#app').append('<br><br><h1>Don\'t get lost in the LABYRINTH !</h1>');
+}
