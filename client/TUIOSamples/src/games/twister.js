2fe85925d281f6f3f40cf088583b702daed38c7e
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 13:29:22 +0100

    Adding the twister game file and launching it

--- client/TUIOSamples/src/games/twister.js
+++ client/TUIOSamples/src/games/twister.js
@@ -2,4 +2,8 @@
  * @author: Adrian PALUMBO  
  */
 
- 
+export default function launchTwister(players)
+{
+   $('#boardView').remove();
+   $('#app').append('<br><br><h1>Damn mah man, you in for the TWISTER game !</h1>');
+}
