10fae519099ee8583037d0f35d7bf880725454c5
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 10:04:05 +0100

    Determining the current player :boom: :muscle:

--- client/TUIOSamples/src/board.js
+++ client/TUIOSamples/src/board.js
@@ -1,13 +1,13 @@
 
  /*eslint-disable */
 
-let _players = [];
+let _players = []; //needed ? I dunno, maybe
 let _currentPlayer;
 
 export default function showBoardView(players)
 {
     _players = players;
-    _currentPlayer = 0;
+    _currentPlayer = null;
     $('#startView').remove();
     $('#app').append('<div id="boardView"> <h1>Let\'s play !</h1>'+
     '<div id="board"></div>'+
@@ -20,30 +20,21 @@ export default function showBoardView(players)
     initializeDice();
     addTiles(10);
     addPlayers(players);
+    determineCurrentPlayer(players);
     turnPlayer();
 }
 
-function addPlayers(players)
+
+function initializeDice()
 {
-    $('#boardView').append('<div id="players"></div>');
-    //Showing the players' score
-    for (let index = 0; index < players.length; index++) {
-        $('#players').append('<p>'+players[index].name +' - '+players[index].score +'</p><br>');     
-    }
-
-    //Adding the players to the board
-    for (let index = 0; index < players.length; index++) {
-        $('#boardView').append('<img width="50" class="playerAvatar" id="player-"'+index +'" src="../assets/avatars/'+ index +'.jpg">')     
-    }
-
-    $('.playerAvatar').height($('#board div').height()/5);
-    $('.playerAvatar').width($('#board div').height()/5);
-
+    $('#dice').css('margin-left', ($(window).width() - $('#dice').width())/2);
+    $('#dice button').prop('disable', false);
+    $('#dice button').on('click', rollDice);
 }
-
 function addTiles(numberOfTiles)
 {
-    for (let index = 0; index < numberOfTiles; index++) {
+    for (let index = 0; index < numberOfTiles; index++) 
+    {
 
         $('#board').append('<div class="boardTile"></div>');
         if(index % 2 != 0)
@@ -56,15 +47,48 @@ function addTiles(numberOfTiles)
             $('.boardTile').last().css('background-color', '#F48FB1');
         }   
     }
-    $('#board').append('<div class="boardTile" id="lastTile"></div>')
+    $('#board').append('<div class="boardTile" id="lastTile"></div>');
+}
+
+function addPlayers(players)
+{
+    $('#boardView').append('<div id="players"></div>');
+    //Showing the players' score
+    for (let index = 0; index < players.length; index++) 
+    {
+        $('#players').append('<p>'+players[index].name +' - '+players[index].score +'</p><br>');     
+    }
+
+    //Adding the players to the board
+    for (let index = 0; index < players.length; index++) 
+    {
+        $('#boardView').append('<img width="50" class="playerAvatar" id="player-"'+index +'" src="../assets/avatars/'+ index +'.jpg">')     
+    }
+
+    $('.playerAvatar').height($('#board div').height()/5);
+    $('.playerAvatar').width($('#board div').height()/5);
 
 }
 
-function initializeDice()
+function determineCurrentPlayer(players)
 {
-    $('#dice').css('margin-left', ($(window).width() - $('#dice').width())/2);
-    $('#dice button').prop('disable', false);
-    $('#dice button').on('click', rollDice);
+    _currentPlayer = null;
+    for (let index = 0; index < players.length; index++) 
+    {
+        if(players[index].played == false)
+        {
+            _currentPlayer = index;
+            break;
+        }        
+    }
+
+    if(_currentPlayer == null)
+    {
+        _currentPlayer = 0;
+        for (let index = 1; index < players.length; index++) {
+            players[index].played = false;            
+        }
+    }
 }
 
 function turnPlayer()
