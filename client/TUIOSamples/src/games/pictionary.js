7df895a24ad3e6af6d110e4514827cef87c943b8
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 13:30:05 +0100

    Adding the pictionary file and launching it

--- client/TUIOSamples/src/games/pictionary.js
+++ client/TUIOSamples/src/games/pictionary.js
@@ -1,3 +1,9 @@
 /**
  * @author: Thomas GILLOT
  */
+
+export default function launchPictionary(players)
+{
+   $('#boardView').remove();
+   $('#app').append('<br><br><h1>How about some PICTIONARY game !</h1>');
+}
