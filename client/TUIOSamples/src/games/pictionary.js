2a6a9cd1b440ca30cdeeb3f764d101156ac71d52
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 13:38:18 +0100

    Going back to the board from pictionary

--- client/TUIOSamples/src/games/pictionary.js
+++ client/TUIOSamples/src/games/pictionary.js
@@ -1,9 +1,18 @@
 /**
  * @author: Thomas GILLOT
  */
+import showBoardView from "../board";
 
 export default function launchPictionary(players)
 {
    $('#boardView').remove();
    $('#app').append('<br><br><h1>How about some PICTIONARY game !</h1>');
+
+   players[1].score = 45;
+
+   setTimeout(function()
+    {
+        //Get back to the board
+        showBoardView(players);
+    }, 1500);
 }
