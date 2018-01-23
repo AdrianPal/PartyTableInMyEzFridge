62d3536d01b24922e276ed4a7d35b82ec7225f50
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 13:38:40 +0100

    Going back to board from labyrinth

--- client/TUIOSamples/src/games/labyrinth.js
+++ client/TUIOSamples/src/games/labyrinth.js
@@ -2,9 +2,17 @@
  * @author: Arnaud ZAGO
  * 
  */
+import showBoardView from "../board";
 
 export default function launchLabyrinth(players)
 {
    $('#boardView').remove();
    $('#app').append('<br><br><h1>Don\'t get lost in the LABYRINTH !</h1>');
+   players[1].score = 45;
+
+   setTimeout(function()
+    {
+        //Get back to the board
+        showBoardView(players);
+    }, 1500);
 }
