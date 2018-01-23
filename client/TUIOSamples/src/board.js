2a364d59aeeb546c268fa23406aff65753f3f9ba
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 06:00:01 +0100

    Adding a roll the dice div :boom: :sparkling_heart:

--- client/TUIOSamples/src/board.js
+++ client/TUIOSamples/src/board.js
@@ -4,32 +4,33 @@
 export default function showBoardView(players)
 {
     $('#startView').remove();
-  $('#app').append('<div id="boardView"> <h1>Let\'s play !</h1>'+
+    $('#app').append('<div id="boardView"> <h1>Let\'s play !</h1>'+
     '<div id="board"></div>'+
-  
-  '</div>');
-  
-  for (let index = 0; index < 10; index++) {
-  
+    '<div id="dice"><button>Roll the dice</button></div>'+
+
+    '</div>');
+
+    for (let index = 0; index < 10; index++) {
+
     $('#board').append('<div class="boardTile"></div>');
     if(index%2 != 0)
     {
-      $('.boardTile').last().css('background-color', '#F44336');
+        $('.boardTile').last().css('background-color', '#F44336');
 
     }
     else 
     {
-      $('.boardTile').last().css('background-color', '#F48FB1');
+        $('.boardTile').last().css('background-color', '#F48FB1');
     }   
 
     }
 
-  $('#boardView').append('<div id="players"></div>');
-   
-  for (let index = 0; index < players.length; index++) {
+    $('#boardView').append('<div id="players"></div>');
+
+    for (let index = 0; index < players.length; index++) {
     $('#players').append('<p>'+players[index].name +' - '+players[index].score +'</p><br>');     
-  }
+    }
 
 }
 
- /*eslint-enable */
+    /*eslint-enable */
