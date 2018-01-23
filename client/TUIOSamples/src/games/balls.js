57427a69b8d3736197bd7e93f1bc06a4c780cbdb
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-23 13:30:40 +0100

    Going back to board view after balls

--- client/TUIOSamples/src/games/balls.js
+++ client/TUIOSamples/src/games/balls.js
@@ -2,8 +2,16 @@
  * @author: Rémy KALOUSTIAN
  */
 
+ import showBoardView from "../board";
+
  export default function launchBalls(players)
  {
     $('#boardView').remove();
     $('#app').append('<br><br><h1>You lucky dude launched the BALLS game !</h1>');
+
+    
+    setTimeout(function()
+    {
+        showBoardView(players);
+    }, 1500);
  }
