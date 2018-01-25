6b45f4d61703da64b5260debb4dc352b517ee7b6
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-25 09:47:22 +0100

    Adding a ball container to the balls game

--- client/TUIOSamples/src/games/balls.js
+++ client/TUIOSamples/src/games/balls.js
@@ -1,14 +1,18 @@
 /**
  * @author: Rémy KALOUSTIAN
  */
+import BallContainer from 'tuiomanager/widgets/Library/LibraryStack/BallContainer';
 
  import showBoardView from "../board";
 
  export default function launchBalls(players)
  {
     $('#boardView').remove();
-    $('#app').append('<br><br><h1>You lucky dude launched the BALLS game !</h1>');
+    $('#app').append('<div id="ballsView"> </div>');
+    $('#ballsView').append('<br><br><h1>You lucky dude launched the BALLS game !</h1>');
 
+    const libstack = new BallContainer(600, 300, 300, 'ma stack', '#C9C9C9', false, []);
+    libstack.addTo($('#ballsView').get(0));
     players[1].score = 45;
     setTimeout(function()
     {
