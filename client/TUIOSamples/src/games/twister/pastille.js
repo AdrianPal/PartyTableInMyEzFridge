1a6ad96239e24026a71c727c10b9e23a18259ac0
Adrian PALUMBO <adrian.pal@me.com>
2018-01-26 10:43:23 +0100

    sds

--- client/TUIOSamples/src/games/twister/pastille.js
+++ client/TUIOSamples/src/games/twister/pastille.js
@@ -1,19 +1,19 @@
 import ElementWidget from 'tuiomanager/widgets/ElementWidget/ElementWidget';
 import ImageElementWidget from '../../../../TUIOManager/widgets/ElementWidget/ImageElementWidget/ImageElementWidget';
 
-export default class Pastille extends ImageElementWidget {
+export default class Pastille extends ElementWidget {
     constructor(x, y, color, parent) {
         super(x, y, 100, 100, 0, 1);
 
         this.parent = parent;
         this.color = color;
 
-        console.log(color);
+        console.log(thicolor);
 
         this._domElem = $('<div>');
         this._domElem.attr('class', 'not-visible pastille ' + this.color);
         this._domElem.attr('data-color', this.color);
-        
+
         this._domElem.css('width', `${this.width}px`);
         this._domElem.css('height', `${this.height}px`);
         this._domElem.css('position', 'absolute');
@@ -23,14 +23,30 @@ export default class Pastille extends ImageElementWidget {
     }
 
     onTouchCreation(tuioTouch) {
-        super.onTouchCreation(tuioTouch);
-
-        this.parent.pastilleTouched(tuioTouch._id, this.color);
+        if (this.isTouched(tuioTouch.x, tuioTouch.y)) {
+            this.parent.pastilleTouched(tuioTouch._id, this.color);
+        }
     }
 
     onTouchDeletion(tuioTouchId) {
-        super.onTouchDeletion(tuioTouchId);
-
+        if (typeof (this._lastTouchesValues[tuioTouchId]) !== 'undefined') {
+            const lastTouchValue = this._lastTouchesValues[tuioTouchId];
+            const x = lastTouchValue.x;
+            const y = lastTouchValue.y;
+            if (!this._isInStack) {
+                Object.keys(TUIOManager.getInstance()._widgets).forEach((widgetId) => {
+                    if (TUIOManager.getInstance()._widgets[widgetId].constructor.name === 'LibraryStack') {
+                        if (this.isInBounds(TUIOManager.getInstance()._widgets[widgetId], x, y) && !TUIOManager.getInstance()._widgets[widgetId].isDisabled && TUIOManager.getInstance()._widgets[widgetId].isAllowedElement(this)) {
+                            this._isInStack = true;
+                            TUIOManager.getInstance()._widgets[widgetId].addElementWidget(this);
+                            return;
+                        }
+                    }
+                });
+            }
+        }
+        ElementWidget.isAlreadyTouched = false;
+        
         this.parent.pastilleUnTouched(tuioTouchId, this.color);
     }
 }
