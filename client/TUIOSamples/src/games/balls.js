dd90f1df74f9593b224d918d432cbeabd1ec62e1
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-27 04:48:20 +0100

    Spawning the balls

--- client/TUIOSamples/src/games/balls.js
+++ client/TUIOSamples/src/games/balls.js
@@ -4,10 +4,17 @@
 import BallContainer from 'tuiomanager/widgets/Library/LibraryStack/BallContainer';
 import Ball from 'tuiomanager/widgets/ElementWidget/ImageElementWidget/Ball';
 
+import SocketManager from "../../socket.manager";
+
+
  import showBoardView from "../board";
+ var socket = SocketManager.get();
 
  export default function launchBalls(players)
  {
+
+    
+
     $('#boardView').remove();
     $('#app').append('<div id="ballsView"> </div>');
     //$('#ballsView').append('<br><br><h1>You lucky dude launched the BALLS game !</h1>');
@@ -15,20 +22,12 @@ import Ball from 'tuiomanager/widgets/ElementWidget/ImageElementWidget/Ball';
    
     players[1].score = 45;
 
-    window.setInterval(function()
-    {
-        const width = $(window).width();
-        const height = $(window).height();
-        const spawnX = Math.random() * (width - 0) + 0;
-        const spawnY = Math.random() * (height - 0) + 0;
-        const mahball = new Ball(spawnX, spawnY, 50, 50, 0, 1, '../../assets/joy.png');
-        mahball.addTo($('#ballsView').get(0));
-    }, 1000);
+   spawnBalls();
 
-    setTimeout(function()
+    /*setTimeout(function()
     {
         showBoardView(players);
-    }, 150000000);
+    }, 150000000);*/
  }
 
  function addBallContainers(players)
@@ -70,4 +69,20 @@ import Ball from 'tuiomanager/widgets/ElementWidget/ImageElementWidget/Ball';
  {
     const libstack = new BallContainer(x, y, width, name, color, false, []);
     libstack.addTo($('#ballsView').get(0));
+ }
+
+ function spawnBalls()
+ {
+    window.setInterval(function()
+    {
+        const width = $(window).width();
+        const height = $(window).height();
+        const spawnX = Math.random() * (width - 0) + 0;
+        const spawnY = Math.random() * (height - 0) + 0;
+        const mahball = new Ball(spawnX, spawnY, 50, 50, 0, 1, '../../assets/joy.png');
+        mahball.addTo($('#ballsView').get(0));
+
+        socket.emit("balls",{stringO: 'Sendin dem balls'});
+        console.log(socket);
+    }, 1000);
  }
