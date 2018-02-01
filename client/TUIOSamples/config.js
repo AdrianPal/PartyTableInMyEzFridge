8fbff912917aecba9accd9f50cdfb25064932a10
thomasgillot <thomas.gillot2@gmail.com>
2018-02-01 11:32:03 +0100

    Add config.js to merge

---
+++ client/TUIOSamples/config.js
@@ -0,0 +1,8 @@
+/* module.exports = URL_SERVER = 'http://10.212.97.185:4000';
+module.exports = IP = '10.212.97.185';
+ */
+module.exports = config = {
+    ip: '10.212.97.185',
+    port: 8080,
+    server: 'http://10.212.97.185:4000'
+}
