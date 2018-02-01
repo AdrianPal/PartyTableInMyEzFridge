c253cb8b5475b264f87035bd05fc172cca78329d
thomasgillot <thomas.gillot2@gmail.com>
2018-02-01 11:34:57 +0100

    config.js

--- client/TUIOSamples/config.js
+++ client/TUIOSamples/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* module.exports = URL_SERVER = 'http://10.212.97.185:4000';
 module.exports = IP = '10.212.97.185';
  */
@@ -7,13 +6,3 @@ module.exports = config = {
     port: 8080,
     server: 'http://10.212.97.185:4000'
 }
-=======
-module.exports = {
-    // API Server
-    server: 'http://172.19.250.228:4000',
-    // Current IP
-    ip: '172.19.250.228',
-    // Current Port
-    port: 3000,
-};
->>>>>>> 62cd57baafcae7ec0086056f301af08f7335e2a1
