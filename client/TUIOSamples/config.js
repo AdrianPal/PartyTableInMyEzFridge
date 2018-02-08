8c8036af62805ddba3fb2525205c32e910c5da79
thomasgillot <thomas.gillot2@gmail.com>
2018-02-08 12:00:31 +0100

    Pic merde

--- client/TUIOSamples/config.js
+++ client/TUIOSamples/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     // API Server
-    server: 'http://192.168.1.26:4000',
+    server: 'http://10.212.97.185:4000',
     // Current IP
-    ip: '192.168.1.26',
+    ip: '10.212.97.185',
     // Current Port
     port: 3000,
 };
