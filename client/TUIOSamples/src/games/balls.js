b19fe092024b12b2b88423d6d8983f61054206cf
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-27 12:14:58 +0100

    Balls refactor

--- client/TUIOSamples/src/games/balls.js
+++ client/TUIOSamples/src/games/balls.js
@@ -12,28 +12,27 @@ import Anywhere from '../tools/anywhere';
 
 const config = require('../../config');
 
-//import SocketManager from "../../socket.manager";
-
-
-// import showBoardView from "../board";
- //var socket = SocketManager.get();
-
+//Settings
  let PLAYSOUND = true;
- let _colors = [];
- let _tags = ['03', '6D', '6C', 'B3'];
- let _containers = [];
- let _players = [];
- let _isGameOver = false;
- let _ballsCount = 0;
- let _gameTime = 40000; //in milliseconds
- let _ballsLifespan = 3500;
- let _bonusFrequency = 5000;
- let _winners = [];
- let _gameID = '';
+ let _colors ;
+ let _containers ;
+ let _players;
+ let _isGameOver;
+ let _ballsCount ;
+ let _gameTime; //in milliseconds
+ let _ballsLifespan ;
+ let _bonusFrequency;
+ let _winners ;
+ let _gameID;
  let _endAnywhere;
+
+ const BALLWIDTH = 75;
+ const ballContainerWidth = 200;
+ const ballContainerHeight = 200;
+
+ //Used when a bonus is touched w/ tangible
  let _bonusHandler = {
      onBonusTouched: function(tag){
-         //console.log("Bonus Touched w/ tag "+ tag);
          for (let index = 0; index < _players.length; index++) {
             if(PLAYSOUND){$('#bonusgainsound')[0].play();}
              
@@ -48,86 +47,48 @@ const config = require('../../config');
      }
  };
 
- const BALLWIDTH = 75;
- const ballContainerWidth = 200;
- const ballContainerHeight = 200;
-
  export default function launchBalls(gameId)
  {
     User.remove();
     initialize();
     _gameID = gameId;
 
-    //Just for tests
-    let players = [
-        {name:'Papalumbo', avatar:'1', score:0, x:0, y:0, played:false, color:'#088a00'},
-        {name:'RHRHRRH', avatar:'2', score:0, x:0, y:0, played:false, color:'#0050ef'},
-        {name:'Zagogogadget', avatar:'3', score:0, x:0, y:0, played:false, color:'#d80073'},
-        {name:'Kastoulian', avatar:'4', score:0, x:0, y:0, played:false, color:'#fa6800'}
-      ]
-
-      //getting the REAL players
-      $.get(config.server + '/api/user/' + gameId)
-      .done(function (d) {
-      
-        players = d;
-
-        User.remove();
-
-        $('#app').append('<div id="ballsView"> '+
-        '<audio  id = "picksound"> <source src="../../assets/sound/picksound.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
-        '<audio  id = "gameoversound"> <source src="../../assets/sound/gameover.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
-        '<audio  id = "bonusspawnsound"> <source src="../../assets/sound/bonusspawn.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
-        '<audio  id = "bonusgainsound"> <source src="../../assets/sound/bonusgain.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
-        '<audio  id = "ballsmusic"> <source src="../../assets/sound/shootingstars.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
-        
-        '</div>');
-
-        //$('#ballsView').append('<button id="tt">TT</button>');
-        getPlayers(players);
+    //getting the REAL players
+    $.get(config.server + '/api/user/' + gameId)
+    .done(function (d) {
     
-       if(PLAYSOUND){ $('#ballsmusic')[0].play();}
-        $('#ballsmusic').prop("volume", 0.3);
-        
-        addBallContainers();
-        addTimeBars();
-        spawnBalls();
-        spawnBonusBalls();
-        setCountdown();
-        triggerTime();
+    let players = d;
 
-      })
-      .fail(function (e) {
-          alert('Error: can\'t get players.');
-      });
+    //Basic view and sounds
+    $('#app').append('<div id="ballsView"> '+
+    '<audio  id = "picksound"> <source src="../../assets/sound/picksound.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
+    '<audio  id = "gameoversound"> <source src="../../assets/sound/gameover.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
+    '<audio  id = "bonusspawnsound"> <source src="../../assets/sound/bonusspawn.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
+    '<audio  id = "bonusgainsound"> <source src="../../assets/sound/bonusgain.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
+    '<audio  id = "ballsmusic"> <source src="../../assets/sound/shootingstars.mp3" type="audio/mpeg">Your browser does not support the audio element. </audio>'+
+    '</div>');
 
+    getPlayers(players);
 
+    if(PLAYSOUND){ $('#ballsmusic')[0].play();}
+    $('#ballsmusic').prop("volume", 0.3);
     
+    addBallContainers();
+    addTimeBars();
+    spawnBalls();
+    spawnBonusBalls();
+    setCountdown();
+    triggerTime();
 
-   //test for adding balls count
-   $('body').on('click','#tt', function()
-    {
-        /*for (let index = 0; index < _containers.length; index++) 
-        {
-            const mahball = new Ball(0, 0, 50, 50, 0, 1, '../../assets/ballt.png', _players[Math.floor(Math.random() * (_players.length))].color);
-            _containers[index].addElementWidget(mahball);   
-        }*/
-
-        /*const mahball = new Ball(0, 0, 50, 50, 0, 1, '../../assets/ballt.png', '#FF6633', _players[2].name);
-        _containers[0].addElementWidget(mahball);   */
-        _bonusHandler.onBonusTouched(_players[0].tag);        
     })
-
-    /*setTimeout(function()
-    {
-        showBoardView(players);
-    }, 150000000);*/
+    .fail(function (e) {
+        alert('Error: can\'t get players.');
+    });
  }
 
  function initialize()
  {
      _colors = [];
-     _tags = ['03', '6D', '6C', 'B3'];
      _containers = [];
      _players = [];
      _isGameOver = false;
@@ -143,8 +104,7 @@ const config = require('../../config');
  function addBallContainers()
  {
     for (let index = 0; index < _players.length; index++) 
-    {
-        
+    {        
         let x = 0;
         let y = 0;
         let rotation = 0;
@@ -152,8 +112,7 @@ const config = require('../../config');
         {
              x = $(window).width()/ 2 - ballContainerWidth/2;
              y = 1;
-			 rotation = 180;
-            
+			 rotation = 180;            
         }
 
         else if(_players[index].position == 'bottom')//bottom container
@@ -181,22 +140,11 @@ const config = require('../../config');
  function addBallContainer(x, y, width, color, name, rotation, index)
  {
     const container = new BallContainer(x, y, width, name, color, false, ['Ball'],rotation, _gameTime);
-    //container.canRotate(false, false);
-    //container.canMove(false, false);
-    //container.canZoom(false, false);
-   // container.canDelete(false, false);
+   
     container.addTo($('#ballsView').get(0));
     _players[index].stack = container;
-    //Just for the tests
     _containers.push(container);
     _players[index].stack.showImg(config.server + '/' +_players[index].img);
-    //$('#ballsView').append('<img src="'+ config.server + '/' +_players[index].img +'"  />')
-/*
-    --------    TESTS   - -----
-
-    const mahball = new Ball(0, 0, 50, 50, 0, 1, '../../assets/ballt.png', '#FF3366');
-    container.addElementWidget(mahball);
-    */
  }
 
  function addTimeBars()
@@ -205,34 +153,30 @@ const config = require('../../config');
      {
          $('#ballsView').append('<p><progress class="timeBar" id="'+_players[index].name +'bar" value="100" max="100"></progress></p>');
          $('.timeBar').width(ballContainerWidth);
+
          if(_players[index].position == 'top')
          {
             $('#' + _players[index].name + 'bar').css('left', _players[index].stack.x - ballContainerWidth +72);
             $('#' + _players[index].name + 'bar').css('top', _players[index].stack.y + 70);
-            
             $('#' + _players[index].name + 'bar').css('transform', 'rotate(90deg)');
-         
         }
          else if(_players[index].position == 'bottom')
          {
             $('#' + _players[index].name + 'bar').css('left', _players[index].stack.x + 130);
             $('#' + _players[index].name + 'bar').css('top', _players[index].stack.y+75);
             $('#' + _players[index].name + 'bar').css('transform', 'rotate(-90deg)');
-                       
          }
          else if(_players[index].position == 'left')
          {
             $('#' + _players[index].name + 'bar').css('top', _players[index].stack.y + ballContainerWidth+3);           
             $('#' + _players[index].name + 'bar').css('left', _players[index].stack.x);
-             
          }
          else if(_players[index].position == 'right')
          {
             $('#' + _players[index].name + 'bar').css('left', _players[index].stack.x);
             $('#' + _players[index].name + 'bar').css('top', _players[index].stack.y-55);           
             $('#' + _players[index].name + 'bar').css('transform', 'rotate(180deg)');
-         }
-         
+         }         
      }
  }
 
@@ -241,13 +185,9 @@ const config = require('../../config');
     window.setInterval(function()
     {
         if(!_isGameOver && _ballsCount < 10)
-        {
-
-        
+        {        
             const width = $(window).width();
-            const height = $(window).height();
-            //const spawnX = Math.random() * ((width - BALLWIDTH)   - 0) + 0;
-            //const spawnY = Math.random() * ((height- BALLWIDTH) - 0) + 0;
+            const height = $(window).height();          
             const coords = getSpawnCoords();
             const spawnX = coords.x;
             const spawnY = coords.y;
@@ -256,27 +196,20 @@ const config = require('../../config');
 			const tag = _players[index].tag;
 
             const mahball = new Ball(spawnX, spawnY, BALLWIDTH, BALLWIDTH, 0, 1, '../../assets/ballt.png', color, _players[index].name);
-			//const mahball = new ImageElementWidget(spawnX, spawnY, 50, 50, 0, 1, '../../assets/ballt.png');
-
             mahball.canRotate(false, false);
             mahball.canMove(true, true);
             mahball.canZoom(false, false);
             mahball.canDelete(false, false);        
-			
             mahball.setTagMove(tag);
             mahball.addTo($('#ballsView').get(0));
+
 			_ballsCount++;
 
-                setTimeout( function()
-				{ 
-					//if(!mahball._isTouched)
-					//{
-						  mahball.destroy();
-						_ballsCount--;
-					//}
-                  
-                }  , _ballsLifespan );
-                
+            setTimeout( function()
+            { 
+                mahball.destroy();
+                _ballsCount--;              
+            }  , _ballsLifespan );                
         }//if
     }, 100);     //setIntervall()   
  }//spawnBalls()
@@ -288,9 +221,7 @@ const config = require('../../config');
         if(!_isGameOver)
         {        
             const width = $(window).width();
-            const height = $(window).height();
-            //const spawnX = Math.random() * ((width - BALLWIDTH)   - 0) + 0;
-            //const spawnY = Math.random() * ((height- BALLWIDTH) - 0) + 0;
+            const height = $(window).height();            
             const coords = getSpawnCoords();
             const spawnX = coords.x;
             const spawnY = coords.y;
@@ -298,53 +229,41 @@ const config = require('../../config');
             const color = _players[index].color;
             const tag = _players[index].tag;
             const mahball = new BonusBall(spawnX, spawnY, BALLWIDTH, BALLWIDTH, 0, 1, '../../assets/bonusicon.png', color, _players[index].name, _bonusHandler);
-			//const mahball = new ImageElementWidget(spawnX, spawnY, 50, 50, 0, 1, '../../assets/ballt.png');
 
             mahball.canRotate(false, false);
             mahball.canMove(true, true);
             mahball.canZoom(false, false);
             mahball.canDelete(false, false);        
-			
             mahball.setTagMove(tag);
             mahball.addTo($('#ballsView').get(0));
 
-            if(PLAYSOUND)
-             {   $('#bonusspawnsound')[0].play();}
+            if(PLAYSOUND) {   $('#bonusspawnsound')[0].play();}
 
 			_ballsCount++;
 
-                setTimeout( function()
-				{ 
-					//if(!mahball._isTouched)
-					//{
-						  mahball.destroy();
-						_ballsCount--;
-					//}
-                  
-                }  , _ballsLifespan );
-               
+            setTimeout( function()
+            { 
+                mahball.destroy();
+                _ballsCount--;
+            }  , _ballsLifespan );            
         }//if
     }, _bonusFrequency);     //setIntervall()   
  }
 
  function getSpawnCoords()
  {
-     let inc = 0;
     const width = $(window).width();
     const height = $(window).height();
     let areCoordsRight = false;
     let x = 0;
     let y = 0;
+
     while(!areCoordsRight)
-    {
-        ++inc;
-        if(inc > 1000)
-        {
-            //break;
-        }
+    {        
         x = Math.random() * ((width - BALLWIDTH)   - 0) + 0;
         y = Math.random() * ((height- BALLWIDTH) - 0) + 0;
         areCoordsRight = true;
+
         for (let index = 0; index < _containers.length; index++) 
         {
             if(!_containers[index].areCoordsRight(x, y, BALLWIDTH, ballContainerWidth))
@@ -353,13 +272,11 @@ const config = require('../../config');
             }        
         }        
     }  
-    
-
      return {x:x, y:y};
  }
 
- function setCountdown()
- {
+function setCountdown()
+{
     setTimeout( function()
     { 
         _isGameOver = true;
@@ -374,13 +291,10 @@ function triggerTime()
     const partToRemove = $('#' + _players[0].name + 'bar').val()/(_gameTime/1000);
 	window.setInterval( function()
 	{ 
-        //_players[0].stack.addBall();
-
         const frameTime = 1000
 		if(_gameTime>0)
 		{
-			_gameTime  -=  frameTime;
-		
+			_gameTime  -=  frameTime;		
 		
             for (let index = 0; index < _containers.length; index++) 
             {
@@ -393,8 +307,7 @@ function triggerTime()
                 const sub = 100 * (frameTime/_gameTime)
                 $('#' + _players[index].name + 'bar').val($('#' + _players[index].name + 'bar').val() - partToRemove);
             }
-		}
-		
+		}		
 	}  , 1000 );	
 }
 
@@ -411,18 +324,15 @@ function triggerTime()
             tag: players[index].tangible,//to get later through API
             score:0, //to get later through API
             img:players[index].avatarPath
-        }
-        
+        }        
     );        
     }
-    console.log(_players)
  }
 
  function displayGameOver()
  {
     $('#ballsmusic')[0].pause();
-    if(PLAYSOUND){$('#gameoversound')[0].play();}
-     
+    if(PLAYSOUND){$('#gameoversound')[0].play();}     
  }
 
  function showWinner()
@@ -449,15 +359,11 @@ function triggerTime()
             _players[index].stack.showOutcome(false);
         }    
     }
-  
-
     updateScores(winner);
  }
 
  function updateScores(winner)
  {
-    
-    
     $.ajax({
         url: config.server + '/api/user/points',
         type: 'PUT',
@@ -474,18 +380,14 @@ function triggerTime()
      _endAnywhere.deleteWidget();
     $('#ballsView').remove();
     new Home(_gameID);
-
  }
 
  function backToBoard()
  {
-     
-
      setTimeout(() => {
      $('#ballsView').append('<h1 id="touch1">Touch anywhere to go back to the menu</h1>');
          
         _endAnywhere = new Anywhere(this, clickAnywhere);
      }, 4000);    
-
  }
  
